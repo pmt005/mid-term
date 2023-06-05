16c088dafad988cd72180feeeb6b0fcaf22febb8
Patrick Tumu <patricktumu@gmail.com>
2023-06-05 12:12:45 -0600

    set routes for /post /saved /listed as well as update box shadow and cursor for buttons

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,28 @@
 // Client facing scripts here
+
+$(document).ready(function() {
+
+  //Function to dynamicaly create a item and return that item
+  const createTweetElement = function(itemObj) {
+
+
+    let $item = $(  `
+      <article class="item">
+        <img class="thumbnail_photo1" src="/temp-images/thumbnail_1 Small.png">
+        <div id="item-info">
+          <span><strong>Price:  </strong>${itemObj.price}</span>
+          <br>
+          <span><strong>Title:  </strong>${itemObj.title}</span>
+          <br>
+          <span><strong>City:  </strong>${itemObj.city}</span>
+        </div>
+      </article>`);
+
+    return $item;
+
+
+
+
+
+  };
+});
