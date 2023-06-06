a3b159d4814569ddcd50269bec1c191ed224cc3e
Patrick Tumu <patricktumu@gmail.com>
2023-06-05 20:34:46 -0600

    post button now toggles to reveal or hide the input form for posting a new ad

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,12 +1,11 @@
+/* eslint-disable no-undef */
 // Client facing scripts here
 
 $(document).ready(function() {
 
   //Function to dynamicaly create a item and return that item
-  const createTweetElement = function(itemObj) {
-
-
-    let $item = $(  `
+  const createItem = function(itemObj) {
+    let $item = $(`
       <article class="item">
         <img class="thumbnail_photo1" src="/temp-images/thumbnail_1 Small.png">
         <div id="item-info">
@@ -19,10 +18,34 @@ $(document).ready(function() {
       </article>`);
 
     return $item;
-
-
-
-
-
   };
+
+
+  //Listener for post new item submit to get post new item form
+  $("#get-post-form").on('click',function(event) {
+    event.preventDefault();
+    const $val = $("#post-form-container");
+    console.log("post button");
+    $val.slideToggle();
+
+  });
+
+  //Listener for get listed items
+  $("#get-listed-items").submit(function(event) {
+    event.preventDefault();
+    console.log("listed button");
+  });
+
+  //Listener for post new item submit to get post new item form
+  $("#get-saved-items").submit(function(event) {
+    event.preventDefault();
+    console.log("saved button");
+  });
+
+
+
+
+
+
+
 });
