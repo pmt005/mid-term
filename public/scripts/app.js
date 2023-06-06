cd52e342a26207d99743a97e9c4f92e2078722ee
Patrick Tumu <patricktumu@gmail.com>
2023-06-06 16:49:51 -0600

    moved the header to a js component and out of index.ejs (move toward SPA)

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,12 +1,10 @@
 /* eslint-disable no-undef */
 // Client facing scripts here
 
-$(document).ready(function() {
-  const $postForm = $("#container-to-vanish-post-form");
-  //const $savedItems = $postForm.parent().find("#saved-items");
-  const $savedItems = $("#container-to-vanish-saved");
+
+
   //Function to dynamicaly create a item and return that item
-  const createItem = function(itemObj) {
+  const createItemThumb = function(itemObj) {
     let $item = $(`
       <article class="item">
         <img class="thumbnail_photo1" src="/temp-images/thumbnail_1 Small.png">
@@ -23,24 +21,9 @@ $(document).ready(function() {
   };
 
 
-  //Listener for post new item submit to get post new item form
-  $("#get-post-form").on('click',function(event) {
-    event.preventDefault();
-    $postForm.slideToggle();
-  });
 
-  //Listener for get listed items
-  $("#get-listed-items").submit(function(event) {
-    event.preventDefault();
-    console.log("listed button");
-  });
 
-  //Listener for post new item submit to get post new item form
-  $("#get-saved-items").submit(function(event) {
-    event.preventDefault();
-    $savedItems.slideToggle();
-    console.log("saved button");
-  });
+
 
 
 
---
+++ public/scripts/components/header.js
@@ -0,0 +1,78 @@
+/* eslint-disable no-undef */
+/* eslint-disable func-style */
+$(() => {
+  window.header = {};
+
+  const $pageHeader = $('#page-header');
+  function updateHeader() {
+
+    let userLinks;
+    userLinks = `
+      <header id="header">
+      <div id="top-left">
+        <div id="icon-title">
+          <img src="/icons/shop.png" id="titleIcon">
+          <span id="title">Mid-term Marketplace</span>
+        </div>
+
+        <span id="creators"><div><strong>GROUP TWO </strong>project eight<br></div></span>
+        <div id="header-footer">
+          <form action="/items" method="GET" id="keyword-search">
+            <textarea name="text" id="search-text" placeholder="search"></textarea>
+          </form>
+          <button type="submit" id="filter-button">filter</button>
+        </div>
+      </div>
+    <div id="top-right">
+      <div id="fav">
+        <form action="/saved" method="GET" id="get-saved-items">
+          <button type="submit" class="user-icon"><img src="/icons/fav.png " id="icon"> </button>
+        <span id="creators"> <strong>Saved<br></strong></span>
+        </form>
+      </div>
+      <div id="fav">
+        <form action="/listed" method="GET" id="get-listed-items">
+          <button type="submit" class="user-icon"><img src="/icons/sales.png" id = "icon"> </button>
+          <span id="creators"> <strong>Listed<br></strong></span>
+        </form>
+      </div>
+      <div id="fav">
+        <form action="/post" method="GET" id="get-post-form">
+          <button type="submit" class="user-icon"><img src="/icons/post.png" id="icon"> </button>
+          <span id="creators"> <strong>Post<br></strong></span>
+        </form>
+      </div>
+    </div>
+    </header>
+
+      `;
+
+    $pageHeader.append(userLinks);
+  }
+  updateHeader();
+  window.header.update = updateHeader;
+
+  $(document).ready(function() {
+    const $postForm = $("#container-to-vanish-post-form");
+    const $savedItems = $postForm.parent().find("#container-to-vanish-saved");
+    //Listener for post new item submit to get post new item form
+    $("#get-post-form").on('click', function(event) {
+      event.preventDefault();
+      $postForm.slideToggle();
+    });
+
+    //Listener for get listed items
+    $("#get-listed-items").submit(function(event) {
+      event.preventDefault();
+      console.log("listed button");
+    });
+
+    //Listener for post new item submit to get post new item form
+    $("#get-saved-items").submit(function(event) {
+      event.preventDefault();
+      $savedItems.slideToggle();
+      console.log("saved button");
+    });
+  })
+
+});
