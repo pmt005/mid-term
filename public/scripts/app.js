4f7658519d551ba0a111245f870648193e0d0989
Patrick Tumu <patricktumu@gmail.com>
2023-06-05 22:08:14 -0600

    completed saved button functionality

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,7 +2,9 @@
 // Client facing scripts here
 
 $(document).ready(function() {
-
+  const $postForm = $("#container-to-vanish-post-form");
+  //const $savedItems = $postForm.parent().find("#saved-items");
+  const $savedItems = $("#container-to-vanish-saved");
   //Function to dynamicaly create a item and return that item
   const createItem = function(itemObj) {
     let $item = $(`
@@ -24,10 +26,7 @@ $(document).ready(function() {
   //Listener for post new item submit to get post new item form
   $("#get-post-form").on('click',function(event) {
     event.preventDefault();
-    const $val = $("#post-form-container");
-    console.log("post button");
-    $val.slideToggle();
-
+    $postForm.slideToggle();
   });
 
   //Listener for get listed items
@@ -39,6 +38,7 @@ $(document).ready(function() {
   //Listener for post new item submit to get post new item form
   $("#get-saved-items").submit(function(event) {
     event.preventDefault();
+    $savedItems.slideToggle();
     console.log("saved button");
   });
 
