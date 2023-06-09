a24b16c64f76d0f4b39970c78be114c1aad7a266
Patrick Tumu <patricktumu@gmail.com>
2023-06-09 09:37:33 -0600

    returning to listed branch

--- public/scripts/components/header.js
+++ public/scripts/components/header.js
@@ -34,7 +34,7 @@ $(document).ready(function() {
         </form>
       </div>
       <div id="fav">
-        <form action="/listed" method="GET" id="get-listed-items">
+      <form action="/api/items/listedItems" method="GET" id="get-listed-items">
           <button type="submit" class="user-icon"><img src="/icons/sales.png" id = "icon"> </button>
           <span id="creators"> <strong>Listed<br></strong></span>
         </form>
@@ -49,7 +49,7 @@ $(document).ready(function() {
     </header>
 
       `;
-
+//
     $pageHeader.append(header);
   }
   window.header.update = updateHeader;
@@ -66,14 +66,6 @@ $(document).ready(function() {
     views_manager.show('newItem');
   });
 
-  //Listener for redirect to home page
-  $("#icon-title").hover(function(event) {
-    $(this).css('cursor', 'pointer');
-  });
-
-  $("#icon-title").on('click', function() {
-    window.location.href="/";
-  });
 
   function getAllItems(inputParam) {
     let url = "/api/items";
@@ -158,6 +150,7 @@ $(document).ready(function() {
 
       $(".item").on('click', function(event) {
         event.preventDefault();
+        console.log("THIS IS THE TEST");
         let tempItemId = event.currentTarget.dataset.id;
 
         console.log(tempItemId[0]);
@@ -171,7 +164,7 @@ $(document).ready(function() {
           const jCity = "<Strong>City :</strong>" + j.city;
           const jComm = "<Strong>Comm :</strong>" + j.community;
           const jProv = "<Strong>Prov :</strong>" + j.province;
-          const jPrice = "<Strong>Price :</strong>" + "$" + (j.price/100).toFixed(2);
+          const jPrice = "<Strong>Price :</strong>" + j.price;
           const jTitle = "<h3>" + j.title + "</h3>";
 
           openModal(jTitle, j.cover_photo_url, j.thumbnail_photo1_url,
@@ -190,14 +183,119 @@ $(document).ready(function() {
 
 
 
+
+    function getMyItems() {
+      let url = "/api/items/listedItems";
+      return $.ajax(url, { method: 'GET' });
+    }
+
+    //Listener for get listed items
+    $("#icon").on('click',function(event) {
+      event.preventDefault();
+      getMyItems().then(function(json) {
+        console.log(json);
+        shallowItemListings.addShallowListings(json.items);
+        views_manager.show('shallowListings');
+
+        const modal = document.querySelector(".modal");
+        const overlay = document.querySelector(".overlay");
+        const $desc = document.querySelector("#mDesc");
+        const textContainer = document.querySelector("#mTitle");
+        const $price = document.querySelector("#mPrice");
+        const $prov = document.querySelector("#mProv");
+        const $city = document.querySelector("#mCity");
+        const $comm = document.querySelector("#mComm");
+
+
+        const closeModal = function() {
+          modal.classList.add("hidden");
+          overlay.classList.add("hidden");
+        };
+
+
+        overlay.addEventListener("click", closeModal);
+
+
+        document.addEventListener("keydown", function(e) {
+          if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+            closeModal();
+          }
+        });
+
+        const getAllItemsId = function(inputParam) {
+          let url = "/api/id";
+          if (inputParam) {
+            url += "/" + inputParam;
+          }
+          console.log("I AM Here11");
+          return $.ajax(url, { method: 'GET' });
+        };
+
+        const openModal = function(title, coverPhoto, thumb1,
+          thumb2, thumb3, desc, price, prov, city, comm) {
+
+          $("#mCover").attr('src', coverPhoto);
+          $("#thumb1").attr('src', thumb1);
+          $("#thumb2").attr('src', thumb2);
+          $("#thumb3").attr('src', thumb3);
+
+
+
+
+          $desc.innerHTML = desc;
+
+          textContainer.innerHTML = title;
+
+          $city.innerHTML = city;
+
+          $comm.innerHTML = comm;
+
+
+
+          console.log("Inner Price:", price);
+          $price.innerHTML = price;
+          $prov.innerHTML = prov;
+          modal.classList.remove("hidden");
+          overlay.classList.remove("hidden");
+        };
+
+
+
+        $(".item").on('click', function(event) {
+          event.preventDefault();
+          let tempItemId = event.currentTarget.dataset.id;
+
+          console.log(tempItemId[0]);
+          const tempItemTitle = event.currentTarget.dataset.title;
+
+          getAllItemsId(tempItemId).then(function(json) {
+            const j = json.items[0];
+            console.log(j.city);
+            console.log(j.community);
+            const jDesc = "<Strong>Description :</strong>" + j.description;
+            const jCity = "<Strong>City :</strong>" + j.city;
+            const jComm = "<Strong>Comm :</strong>" + j.community;
+            const jProv = "<Strong>Prov :</strong>" + j.province;
+            const jPrice = "<Strong>Price :</strong>" + j.price;
+            const jTitle = "<h3>" + j.title + "</h3>";
+
+            openModal(jTitle, j.cover_photo_url, j.thumbnail_photo1_url,
+              j.thumbnail_photo2_url, j.thumbnail_photo3_url, jDesc,
+              jPrice, jProv, jCity, jComm);
+          });
+
+
+
+
+        });
+
+
+
+      });
+    });
   });
 
-  //Listener for get listed items
-  $("#get-listed-items").submit(function(event) {
-    event.preventDefault();
-    userId = cookies.userId;
-    console.log("this is the userId :", userId);
-  });
+
 
   //Listener for saved items
   $("#get-saved-items").submit(function(event) {
@@ -207,10 +305,7 @@ $(document).ready(function() {
   });
 
 
-
-
 });
-
 
 
 
