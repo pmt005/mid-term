84de1e40c7d2ab5a5ae384e9d0ab4417c91b5705
zhihao-qiu <zhihao.qiu@hotmail.com>
2023-06-09 04:24:39 -0700

    listed

--- public/scripts/components/header.js
+++ public/scripts/components/header.js
@@ -34,7 +34,7 @@ $(document).ready(function() {
         </form>
       </div>
       <div id="fav">
-        <form action="/listed" method="GET" id="get-listed-items">
+        <form id="get-listed-items">
           <button type="submit" class="user-icon"><img src="/icons/sales.png" id = "icon"> </button>
           <span id="creators"> <strong>Listed<br></strong></span>
         </form>
@@ -192,11 +192,75 @@ $(document).ready(function() {
 
   });
 
+
+
   //Listener for get listed items
   $("#get-listed-items").submit(function(event) {
-    event.preventDefault();
-    userId = cookies.userId;
-    console.log("this is the userId :", userId);
+    console.log(json.items);
+    shallowItemListings.addShallowListings(json.items);
+    views_manager.show('shallowListings');
+
+    const modal = document.querySelector(".modal");
+    const overlay = document.querySelector(".overlay");
+    const $desc = document.querySelector("#mDesc");
+    const textContainer = document.querySelector("#mTitle");
+    const $price = document.querySelector("#mPrice");
+    const $prov = document.querySelector("#mProv");
+    const $city = document.querySelector("#mCity");
+    const $comm = document.querySelector("#mComm");
+
+
+    const closeModal = function() {
+      modal.classList.add("hidden");
+      overlay.classList.add("hidden");
+    };
+
+
+    overlay.addEventListener("click", closeModal);
+
+
+    document.addEventListener("keydown", function(e) {
+      if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+        closeModal();
+      }
+    });
+
+    const getAllItemsId = function(inputParam) {
+      let url = "/api/id";
+      if (inputParam) {
+        url += "/" + inputParam;
+      }
+      console.log("I AM Here11");
+      return $.ajax(url, { method: 'GET' });
+    };
+
+    const openModal = function(title, coverPhoto, thumb1,
+      thumb2, thumb3, desc, price, prov, city, comm) {
+
+      $("#mCover").attr('src', coverPhoto);
+      $("#thumb1").attr('src', thumb1);
+      $("#thumb2").attr('src', thumb2);
+      $("#thumb3").attr('src', thumb3);
+
+
+
+
+      $desc.innerHTML = desc;
+
+      textContainer.innerHTML = title;
+
+      $city.innerHTML = city;
+
+      $comm.innerHTML = comm;
+
+
+
+      console.log("Inner Price:", price);
+      $price.innerHTML = price;
+      $prov.innerHTML = prov;
+      modal.classList.remove("hidden");
+      overlay.classList.remove("hidden");
+}
   });
 
   //Listener for saved items
